2651001db41bb9ad875d4693bb9ac66b1e98510d
PapryczkQ12 <pstryczek41@gmail.com>
2025-04-11 18:50:38 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,24 @@
+const CACHE_NAME = "app-cache-v1";
+const urlsToCache = [
+  "id.html",
+  "manifest.json",
+  "https://i.imgur.com/n4zJhma.png",
+  "style.css", // Dodaj inne zasoby, np. CSS, JS
+  "script.js"
+];
+
 self.addEventListener("install", (event) => {
-  console.log("Service Worker zainstalowany.");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
